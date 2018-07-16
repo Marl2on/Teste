4ef89291a7321dd5308e1b3ef2a26d33438c3804
Marllon <marllon@gmail.com>
2018-07-16 17:17:54 -0300

    a

--- aura/historicoFiltrosBuscaShoppingLucHead/historicoFiltrosBuscaShoppingLucHeadController.js
+++
@@ -1,6 +0,0 @@
-({
-	irTipoBusca : function(component, event, helper) {
-		var irTipoBuscaEvent = component.getEvent("showTipoBusca");
-        irTipoBuscaEvent.fire();
-	}
-})
--- aura/historicoVenda/historicoVendaRenderer.js
+++
@@ -1,9 +0,0 @@
-({
-    /*afterRender : function(component, helper) {
-       this.superAfterRender();
-       var targetEl = component.find("mainapp").getElement();
-       targetEl.addEventListener("touchmove", function(e) {
-            e.stopPropagation();
-        }, false); 
-    }*/
-})
